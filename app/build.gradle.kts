e1613685f1187f937ae99cd5db04e55e3614bd9e
grehochka82 <lobodina0607@mail.ru>
2026-04-20 22:04:51 +0300

    Step 4: Create HomeScreen with subject list

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation(libs.play.services.mlkit.subject.segmentation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
